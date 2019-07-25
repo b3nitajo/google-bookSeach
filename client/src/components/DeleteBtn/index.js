aefce7f722e5118f51aab931e6932ab44a20a90a
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-07-25 15:04:30 -0500

    working on delete

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -5,9 +5,9 @@ import "./style.css";
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-   // <span className="delete-btn" {...props} role="button" tabIndex="0">
-    <span className="delete-btn" onClick={() => props.removeBook(props.id)} role="button" tabIndex="0">
-      ✗
+   // <span lassName="delete-btn" {...props} role="button" tabIndex="0">
+   <span className="delete-btn" onClick={() => props.removeBook(props.id)}>
+     ✗
     </span>
   );
 }
--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -11,13 +11,13 @@ class Books extends Component {
     books: []
   };
 
-  //removeBook = (id) => {
-   // API.deleteBook(id)
-    //  .then(res => this.setState({ books: res.data }))
-     // .catch(err => console.log(err));
-  removeBook = id => { 
+  removeBook = id => {
+    //API.deleteBook(id);
+     // .then(res => this.setState({ books: res.data }))
+   //   .catch(err => console.log(err));
+ // removeBook = (id) => { 
     // Filter this.state.books for books with an id not equal to the id being removed
-    const books = this.state.books.filter(book => book._id !== id);
+    const books = this.state.books.filter(book => book.id !== id);
     // Set this.state.books equal to the new books array
     this.setState({ books });
   };
@@ -62,7 +62,7 @@ class Books extends Component {
                         {book.title} by {book.author}
                       </strong>
                     </a>
-                    <DeleteBtn removeBook={this.removeBook} />
+                    <DeleteBtn removeBook={this.removeBook}/>
                   </ListItem>
                 ))}
               </List>
