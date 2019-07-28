30d9605674d69c6bd098d397c2e2fbf344c92844
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-07-28 00:38:52 -0500

    deployment changes

---
+++ client/build/precache-manifest.1ee52ba09cb6437323cfd50745051996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94d6fdbfbbf06e0438a6",
+    "url": "/static/css/main.672358df.chunk.css"
+  },
+  {
+    "revision": "94d6fdbfbbf06e0438a6",
+    "url": "/static/js/main.e503dd59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ed8906a2e0f9cadc410",
+    "url": "/static/js/2.9ffc1cf1.chunk.js"
+  },
+  {
+    "revision": "ee208d09344d001deff4242b55d33673",
+    "url": "/index.html"
+  }
+];
