62ec4eb7c6a4e273f86ccfc0cc417c6de74f557d
Benita Jones <47437893+b3nitajo@users.noreply.github.com>
2019-07-28 16:17:30 -0500

    build changes

---
+++ client/build/precache-manifest.1289d45072f06b2863a514ce854c9279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3387eecbf4cf7f4f6df78dd5031b63de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a0439854f1b8278206b",
+    "url": "/static/css/main.e247714c.chunk.css"
+  },
+  {
+    "revision": "ab20451fb87d380bc2d3",
+    "url": "/static/js/2.0630222d.chunk.js"
+  },
+  {
+    "revision": "6a0439854f1b8278206b",
+    "url": "/static/js/main.30f4d267.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
